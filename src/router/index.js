d876ed97fc719593872553c16d3de923e1cb2a72
dongfang <1>
2023-06-12 15:27:48 +0800

    feat(基建)：新增基建、添加路由

---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: {
+      title: "你好",
+      keyword: "about",
+      description: "about",
+    },
+  },
+  {
+    path: "/about",
+    name: "about",
+    meta: {
+      title: "about",
+      keyword: "about",
+      description: "about",
+    },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  next();
+});
+export default router;
